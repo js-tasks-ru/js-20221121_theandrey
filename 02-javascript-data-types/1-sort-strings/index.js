353b181d602b480fdfa614ae1b40a9b325fc4c02
TheAndrey <me@theandrey.name>
2022-11-25 13:56:02 +0200

    Implemented sortStrings()

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,20 @@
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
- * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+ * @param {string} [order="asc"] - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, order = 'asc') {
+  const collator = new Intl.Collator('ru', {caseFirst: 'upper'});
 
+  let comparator;
+  if (order === 'asc') {
+    comparator = collator.compare;
+  } else if (order === 'desc') {
+    comparator = (a, b) => collator.compare(b, a);
+  } else {
+    throw new Error('Unknown order: ' + order);
+  }
+
+  return [...arr].sort(comparator);
 }
